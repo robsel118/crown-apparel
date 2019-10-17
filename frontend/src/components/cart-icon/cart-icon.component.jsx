9876e1a296cc8b0cab75e9371d88117f28b6ef28
robsel118 <robertleeseligmann@gmail.com>
2019-10-17 10:44:50 -0700

    added reselect libray

--- frontend/src/components/cart-icon/cart-icon.component.jsx
+++ frontend/src/components/cart-icon/cart-icon.component.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { Badge } from 'antd';
 import { connect } from 'react-redux';
 import { toggleCartVisibility } from '../../redux/cart/cart.actions';
+import { selectCartItemCount } from '../../redux/cart/cart.selectors';
 import './cart-icon.styles.sass';
 
-const CartIcon = ({ toggleCartVisibility }) => (
-  <div className="cart-icon" onClick={toggleCartVisibility}>
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
-  </div>
+const CartIcon = ({ toggleCartVisibility, itemCount }) => (
+  <Badge count={itemCount} offset={[-10, 10]}>
+    <div className="cart-icon" onClick={toggleCartVisibility}>
+      <ShoppingIcon className="shopping-icon" />
+    </div>
+  </Badge>
 );
+
+const mapStateToProps = state => ({
+  itemCount: selectCartItemCount(state)
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartVisibility: () => dispatch(toggleCartVisibility())
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
